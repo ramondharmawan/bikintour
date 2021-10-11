47f9c0c39c2b7b90634678e628b4882ba3141c43
raycoco <7776848-raycoco@users.noreply.gitlab.com>
2021-10-11 23:27:19 +0800

    editing setting database

--- bikintour/bikintour/settings.py
+++ bikintour/bikintour/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'bikintour.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.psotgressql',
+        'NAME': 'bikintour_db',
+        'USER': 'postgres',
+        'PASSWORD': 'Postgres123',
+        "HOST": 'localhost',
     }
 }
 
